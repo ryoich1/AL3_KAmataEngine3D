1ab11b59b41095ea0179d7f62fdbb35ff99a2d68
ryoich1 <k024g1007@g.neec.ac.jp>
2025-04-14 11:19:06 +0900

    kamataengine導入

---
+++ DirectXGame/GameScene.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <KamataEngine.h>
+class GameScene 
+{
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+};
---
+++ DirectXGame/main.cpp
@@ -0,0 +1,33 @@
+#include <Windows.h>
+#include <KamataEngine.h>
+#include "GameScene.h"
+
+using namespace KamataEngine;
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+	
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+	GameScene* gameScene = new GameScene();
+
+	KamataEngine::Initialize(L"GC2C_11_モトハシ_リョウイチ");
+
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+
+		gameScene->Update();
+
+		dxCommon->PreDraw();
+
+		gameScene->Draw();
+
+		dxCommon->PostDraw();
+	};
+
+	delete gameScene;
+	gameScene = nullptr;
+	KamataEngine::Finalize();
+
+	return 0;
+}
